115146875f0f11d29e1325b9eec532dc83b2edf9
michaelescue <mescue@pdx.edu>
2020-12-06 19:49:52 -0800

    Cleaned Code

--- gesture-recognition-master/recognize.py
+++ gesture-recognition-master/recognize.py
@@ -36,6 +36,7 @@ def foo():
     print ("Counter is %d" % foo.counter)
     return foo.counter
 foo.counter = 0
+Cleane
 #--------------------------------------------------
 # To find the running average over the background
 #--------------------------------------------------
@@ -70,65 +71,6 @@ def segment(image, threshold=50, thresholdtype=cv2.THRESH_BINARY):
         # based on contour area, get the maximum contour which is the hand
         segmented = max(cnts, key=cv2.contourArea)
         return (thresholded, segmented)
-
-#--------------------------------------------------------------
-# To count the number of fingers in the segmented hand region
-#--------------------------------------------------------------
-def count(thresholded, segmented):
-    # find the convex hull of the segmented hand region
-    chull = cv2.convexHull(segmented)
-
-    # find the most extreme points in the convex hull
-    extreme_top    = tuple(chull[chull[:, :, 1].argmin()][0])
-    extreme_bottom = tuple(chull[chull[:, :, 1].argmax()][0])
-    extreme_left   = tuple(chull[chull[:, :, 0].argmin()][0])
-    extreme_right  = tuple(chull[chull[:, :, 0].argmax()][0])
-
-    # find the center of the palm
-    cX = int((extreme_left[0] + extreme_right[0]) / 2)
-    cY = int((extreme_top[1] + extreme_bottom[1]) / 2)
-
-    # find the maximum euclidean distance between the center of the palm
-    # and the most extreme points of the convex hull
-    distance = pairwise.euclidean_distances([(cX, cY)], Y=[extreme_left, extreme_right, extreme_top, extreme_bottom])[0]
-    maximum_distance = distance[distance.argmax()]
-
-    # calculate the radius of the circle with 80% of the max euclidean distance obtained
-    radius = int(0.8 * maximum_distance)
-
-    # find the circumference of the circle
-    circumference = (2 * np.pi * radius)
-
-    # take out the circular region of interest which has 
-    # the palm and the fingers
-    circular_roi = np.zeros(thresholded.shape[:2], dtype="uint8")
-	
-    # draw the circular ROI
-    cv2.circle(circular_roi, (cX, cY), radius, 255, 1)
-
-    # take bit-wise AND between thresholded hand using the circular ROI as the mask
-    # which gives the cuts obtained using mask on the thresholded hand image
-    circular_roi = cv2.bitwise_and(thresholded, thresholded, mask=circular_roi)
-
-    # compute the contours in the circular ROI
-    (cnts, _) = cv2.findContours(circular_roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    # initalize the finger count
-    count = 0
-
-    # loop through the contours found
-    for c in cnts:
-        # compute the bounding box of the contour
-        (x, y, w, h) = cv2.boundingRect(c)
-
-        # increment the count of fingers only if -
-        # 1. The contour region is not the wrist (bottom area)
-        # 2. The number of points along the contour does not exceed
-        #     25% of the circumference of the circular ROI
-        if ((cY + (cY * 0.25)) > (y + h)) and ((circumference * 0.25) > c.shape[0]):
-            count += 1
-
-    return count
 
 #-----------------
 # MAIN FUNCTION
@@ -167,9 +109,6 @@ if __name__ == "__main__":
     # Import Training images
     imdata, err = imimp(path)
 
-    # Import Test image
-    # testdata, err = imimp(testpath)
-
     # Check images properly imported.
     # print(imdata.domain)
     # print(imdata)
@@ -182,9 +121,6 @@ if __name__ == "__main__":
     # Embed Training images
     imembdata, skippedim, numskippedim = imemb(imdata, col="image")
 
-    # Embed test image
-    # testemb, skippedim, numskippedim = imemb(testdata, col="image")
-
     # print(imembdata)
     # print(skippedim)
     # print(numskippedim)
@@ -194,24 +130,11 @@ if __name__ == "__main__":
     # learner = classification.TreeLearner
     learner = classification.KNNLearner()
 
-
     # Train learner for model
-    # lmodel = learner(imembdata)
     lmodel = learner(imembdata)
     
     # Set object for getting class values from data based on prediction
     classval = imembdata.domain.class_var.values
-
-    # Make prediction using learner
-    # prediction = lmodel(testemb)
-
-    # Display prediction
-    # print(classval[int(prediction)])
-
-    # Display tree
-    # printedtree = lmodel.print_tree()
-    # for i in printedtree.split('\n'):
-    #     print(i)
 
     # Set Thresholding type flag
     threshtype = cv2.THRESH_BINARY
